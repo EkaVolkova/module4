efdfaaa08e426ec25c43cf71b075682f742f1bf3
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-07 22:06:34 +0500

    Задание 4.1.17 Перевела все на switch/case

--- module4.1.17/Program.cs
+++ module4.1.17/Program.cs
@@ -11,29 +11,28 @@ namespace module4._1._17
 				Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
 				var color = Console.ReadLine();
+				switch (color)
+                {
+					case "red":
+						Console.BackgroundColor = ConsoleColor.Red;
+						Console.ForegroundColor = ConsoleColor.Black;
 
-				if (color == "red")
-				{
-					Console.BackgroundColor = ConsoleColor.Red;
-					Console.ForegroundColor = ConsoleColor.Black;
+						Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+						Console.BackgroundColor = ConsoleColor.Green;
+						Console.ForegroundColor = ConsoleColor.Black;
 
-					Console.WriteLine("Your color is red!");
+						Console.WriteLine("Your color is green!");
+						break;
+					default:
+						Console.BackgroundColor = ConsoleColor.Cyan;
+						Console.ForegroundColor = ConsoleColor.Black;
+
+						Console.WriteLine("Your color is cyan!");
+						break;
 				}
 
-				else if (color == "green")
-				{
-					Console.BackgroundColor = ConsoleColor.Green;
-					Console.ForegroundColor = ConsoleColor.Black;
-
-					Console.WriteLine("Your color is green!");
-				}
-				else
-				{
-					Console.BackgroundColor = ConsoleColor.Cyan;
-					Console.ForegroundColor = ConsoleColor.Black;
-
-					Console.WriteLine("Your color is cyan!");
-				}
 			}
 		}
 	}
