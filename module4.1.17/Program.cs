7896dd44931d3e93cbaaa42f174146dcf338844b
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-07 22:08:26 +0500

    Модуль 4.1.18: Добавьте в программу обработку ввода цвета cyan, а в блоке default поменяйте на желтый цвет цвет фона, а цвет текста — на красный

--- module4.1.17/Program.cs
+++ module4.1.17/Program.cs
@@ -25,11 +25,17 @@ namespace module4._1._17
 
 						Console.WriteLine("Your color is green!");
 						break;
-					default:
+					case "cyan":
 						Console.BackgroundColor = ConsoleColor.Cyan;
 						Console.ForegroundColor = ConsoleColor.Black;
 
 						Console.WriteLine("Your color is cyan!");
+						break;
+					default:
+						Console.BackgroundColor = ConsoleColor.Yellow;
+						Console.ForegroundColor = ConsoleColor.Red;
+
+						Console.WriteLine("Your color is yellow!");
 						break;
 				}
 
