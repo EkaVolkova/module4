11e48d80d1c919834fa354bb0409e06c1d2e4df4
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-10 21:01:53 +0500

    4.3.11: Выводы массива:
    1. при котором перебор начинается по строкам
    2. при котором перебор начинается по столбцам

--- Array/Program.cs
+++ Array/Program.cs
@@ -21,6 +21,27 @@ namespace Array
                 Console.Write(name[i] + " ");
             }
 
+            //Двумерный массив
+            Console.WriteLine("\n\nДвумерный массив");
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
+            Console.WriteLine("Нормальная форма");
+            //Нормальное отображение
+            for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+            {
+                for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+                    Console.Write(array[i, k] + " ");
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("Транспонирование");
+            //Нормальное отображение
+            for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+            {
+                for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+                    Console.Write(array[k, i] + " ");
+
+                Console.WriteLine();
+            }
         }
     }
 }
