c0f6fa21f8361d135d935e388ea413aec11091b3
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-10 20:46:50 +0500

    Между 4.3.4 и 4.3.5: обратиться к символам в строке, как к элементам массива

---
+++ Array/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введеите свое имя");
+            string name = Console.ReadLine();
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write(name[i] + " ");
+            }
+            Console.WriteLine($"Последняя буква вашего имени {name[^1]}");
+        }
+    }
+}
