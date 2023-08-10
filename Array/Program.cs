020e9ff5fbed42a0f80fddee44e3f5d8a817469d
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-10 20:59:39 +0500

    4.3.7 Буквы имени в обратном порядке

--- Array/Program.cs
+++ Array/Program.cs
@@ -8,11 +8,19 @@ namespace Array
         {
             Console.WriteLine("Введеите свое имя");
             string name = Console.ReadLine();
+            //Вывести в прямом порядке
             for (int i = 0; i < name.Length; i++)
             {
                 Console.Write(name[i] + " ");
             }
             Console.WriteLine($"Последняя буква вашего имени {name[^1]}");
+
+            //Вывести в обратном порядке
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                Console.Write(name[i] + " ");
+            }
+
         }
     }
 }
