791daa7a826db39b4bc76394627e3380496711fa
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-13 15:42:16 +0500

    4.4.5 Анкета питомца

--- tuples/Program.cs
+++ tuples/Program.cs
@@ -15,6 +15,17 @@ namespace tuples
             age = int.Parse(Console.ReadLine());
             Console.WriteLine("Твое имя {0}\nТебе {1} лет", name, age);
 
+            (string Name, string Type, double Age, int NameCount) Pet;
+            Console.WriteLine("Введите имя своего питомца");
+            Pet.Name = Console.ReadLine();
+            Console.WriteLine("Введите тип своего питомца");
+            Pet.Type = Console.ReadLine();
+            Console.WriteLine("Введите возраст своего питомца");
+            Pet.Age = double.Parse(Console.ReadLine());
+            Pet.NameCount = Pet.Name.Length;
+            Console.WriteLine("Имя питомца: {0}\nТип питомца: {1}\nВозраст питомца: {2}\nКоличество символов в имени: {3}", Pet.Name, Pet.Type, Pet.Age, Pet.NameCount);
+
+
         }
     }
 }
