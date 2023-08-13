185408babe2167b84f9d9eeae1139c7ff34a92b1
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-13 15:28:31 +0500

    4.4.2 Модифицируйте свою программу для ввода личной информации пользователя так, чтобы данные записывались в кортеж anketa

---
+++ tuples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, int age) anketa;
+            Console.WriteLine("Введите свое имя");
+            anketa.name = Console.ReadLine();
+            Console.WriteLine("Введите свой возраст");
+            anketa.age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Твое имя {0}\nТебе {1} лет", anketa.name, anketa.age);
+
+        }
+    }
+}
